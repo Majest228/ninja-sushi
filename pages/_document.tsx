853f2090f136633f15b7c1c633846eca81dccdd7
Dmitry <majest228@gmail.com>
2023-03-29 12:09:50 +0600

    Updated Delivery

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <title>KZFL</title>
+        <title>Ninja Sushi</title>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
